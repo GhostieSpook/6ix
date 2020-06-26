cc94e8f0fb593d4955a1dbf65272c9730ef28f1b
Pixxth <61689690+Pixxth@users.noreply.github.com>
2020-06-26 16:59:10 -0400

    Update 6ix.py

--- 6ix.py
+++ 6ix.py
@@ -74,26 +74,26 @@ def startprint():
 
     print(f'''{Fore.RESET}
 
+                                            
+                                              ██████  ██▓▒██   ██▒
+                                            ▒██    ▒ ▓██▒▒▒ █ █ ▒░
+                                            ░ ▓██▄   ▒██▒░░  █   ░
+                                              ▒   ██▒░██░ ░ █ █ ▒ 
+                                            ▒██████▒▒░██░▒██▒ ▒██▒
+                                            ▒ ▒▓▒ ▒ ░░▓  ▒▒ ░ ░▓ ░
+                                            ░ ░▒  ░ ░ ▒ ░░░   ░▒ ░
+                                            ░  ░  ░   ▒ ░ ░    ░  
+                                                ░   ░   ░    ░  
+                                          
                       
 
-
-{Fore.RED}                                                 {Fore.WHITE} ██████  {Fore.WHITE} ██{Fore.RED}▓▒{Fore.WHITE} ██   ██{Fore.RED}▒
-{Fore.RED}                                               ▒{Fore.WHITE} ██    {Fore.RED}▒ ▓{Fore.WHITE} ██{Fore.RED}▒▒▒ {Fore.WHITE} █ █{Fore.RED} ▒░
-{Fore.RED}                                               ░ ▓{Fore.WHITE} ██▄{Fore.RED}   ▒{Fore.WHITE} ██{Fore.RED}▒░░  {Fore.WHITE} █{Fore.RED}   ░
-{Fore.RED}                                               ▒     {Fore.WHITE} ██{Fore.RED}▒░{Fore.WHITE} ██{Fore.RED}░ ░ {Fore.WHITE} █ █{Fore.RED} ▒ 
-{Fore.RED}                                               ▒{Fore.WHITE} ██████{Fore.RED}▒▒░{Fore.WHITE} ██{Fore.RED}░▒{Fore.WHITE} ██{Fore.RED}▒ ▒{Fore.WHITE}██{Fore.RED}▒
-{Fore.RED}                                                 ▒ ▒▓▒ ▒ ░░▓  ▒▒ ░ ░▓ ░
-{Fore.RED}                                              ░ ░▒  ░ ░ ▒ ░░░   ░▒ ░
-{Fore.RED}                                               ░  ░  ░   ▒ ░ ░    ░  
-{Fore.RED}                                                   ░   ░   ░    ░  
-                      
 
                                                   
                                 {Fore.RED}Loggin =>  {Fore.RED}{SIX.user.name}#{SIX.user.discriminator}{Fore.LIGHTYELLOW_EX}  ||{Fore.LIGHTYELLOW_EX}  {Fore.RED}ID => {Fore.RED}{SIX.user.id}   
                                 {Fore.RED}Privnote => {Fore.RED}{privnote}{Fore.LIGHTYELLOW_EX} ||  {Fore.RED}Nitro => {Fore.RED}{nitro} 
                                 {Fore.RED}Giveaway => {Fore.RED}{giveaway}{Fore.LIGHTYELLOW_EX}   ||  {Fore.RED}SlotBot => {Fore.RED}{slotbot}
                                 {Fore.RED}Prefix => {Fore.GREEN}{prefix}{Fore.LIGHTYELLOW_EX}           ||  {Fore.RED}6ix =>{Fore.LIGHTYELLOW_EX} v{SELFBOT.__version__}                                                     
-     '''+Fore.RESET)
+     '''.replace('░', f'{Fore.RED}░{Fore.RESET}').replace('▒', f'{Fore.RED}░{Fore.RESET}').replace('▓', f'{Fore.RED}░{Fore.RESET}')+Fore.RESET)
 
 def Clear():
     os.system('cls')
